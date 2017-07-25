6c2a89c3bef7631e7a4328cd37446e826e63732a
Diego <dondesigner@hotmail.com>
2017-07-24 22:05:20 -0300

    Componentizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//root
 import { AppComponent } from './app.component';
+
+//shareds
 import { HeadBarComponent } from './components/shared/head-bar/head-bar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+//pages
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from  './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadBarComponent
+    HeadBarComponent,
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
